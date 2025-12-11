351cd6da06b6087b5ea790dcf6061f2ac54f438b
RAPCM <ricardo.apc.martins@gmail.com>
2025-12-11 14:28:53 +0100

    typescript menu error solved

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,10 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient as PrismaClientClass } from "@prisma/client";
 
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClientClass({ adapter });
 
 export { prisma };
